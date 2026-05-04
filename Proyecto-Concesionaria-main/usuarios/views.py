23321b704a4b6b485dda036c9a3895137e942633
Celeste040205 <inu-kun_2005@hotmail.com>
2026-05-03 23:04:49 -0500

    correcion idioma :sparkles:

--- Proyecto-Concesionaria-main/usuarios/views.py
+++ Proyecto-Concesionaria-main/usuarios/views.py
@@ -9,8 +9,7 @@ from .forms import RegistroForm, LoginForm
 def inicio(request):
     if request.user.is_authenticated:
         return redirect('dashboard')
-    else:
-        return redirect('login')
+    return redirect('login')
 
 @never_cache
 def registro(request):
@@ -26,6 +25,8 @@ def registro(request):
                 'registro_exitoso': True
             })
         else:
+            # Al fallar el is_valid(), Django adjunta los errores al 'form'
+            # Si configuraste settings.py en español, estos errores saldrán en español.
             return render(request, 'usuarios/registro.html', {
                 'form': form,
                 'registro_fallido': True
@@ -64,10 +65,8 @@ def cerrar_sesion(request):
 @never_cache
 @login_required
 def dashboard(request):
-    # Diccionario base con los datos del usuario logueado
     contexto = {'usuario': request.user}
     
-    # Si es admin, le mandamos la lista de TODOS los usuarios (excepto él mismo para que no se auto-elimine)
     if request.user.is_superuser or request.user.es_administrador:
         contexto['lista_usuarios'] = Usuario.objects.exclude(id=request.user.id)
         
@@ -76,11 +75,9 @@ def dashboard(request):
 @never_cache
 @login_required
 def eliminar_usuario(request, user_id):
-    # Validamos por seguridad que solo los administradores puedan borrar
     if not (request.user.is_superuser or request.user.es_administrador):
         return redirect('dashboard')
     
-    # Solo aceptamos peticiones POST por seguridad (evita que se borren por escribir la URL)
     if request.method == 'POST':
         usuario_a_eliminar = get_object_or_404(Usuario, id=user_id)
         usuario_a_eliminar.delete()
@@ -90,14 +87,11 @@ def eliminar_usuario(request, user_id):
 @never_cache
 @login_required
 def toggle_estado_usuario(request, user_id):
-    # Validamos que solo los administradores puedan hacer esto
     if not (request.user.is_superuser or request.user.es_administrador):
         return redirect('dashboard')
     
     if request.method == 'POST':
         usuario_a_modificar = get_object_or_404(Usuario, id=user_id)
-        
-        # Invertimos el estado actual (si es True pasa a False, y viceversa)
         usuario_a_modificar.is_active = not usuario_a_modificar.is_active
         usuario_a_modificar.save()
         
